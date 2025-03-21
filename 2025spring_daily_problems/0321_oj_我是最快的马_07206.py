9672a082ea6cb985f015369f83a5d944c259e368
twj-ink <287631943@qq.com>
2025-03-22 00:29:20 +0800

    Upload by PyCharm

---
+++ 2025spring_daily_problems/0321_oj_我是最快的马_07206.py
@@ -0,0 +1,56 @@
+from collections import deque
+x,y=map(int,input().split())
+sx,sy=x,y
+ex,ey=map(int,input().split())
+m=int(input())
+s=[[0]*10 for _ in range(10)]
+ss=[[list() for _ in range(10)] for _ in range(10)]
+for _ in range(m):
+    i,j=map(int,input().split())
+    s[i][j]=1
+
+cnt=0
+paths=[]
+dx,dy=[-1,1,-2,2,-2,2,-1,1],[-2,-2,-1,-1,1,1,2,2]
+def canmove(x,y,dx,dy):
+    return s[x+dx//2][y+dy//2]!=1
+def getpath(x,y,curr):
+    # print(ss[x][y])
+    # print(curr)
+    if curr and curr[-1]==(sx,sy):
+        return curr[::-1]
+    curr.append((x,y))
+    nx,ny=ss[x][y][0]
+    # print((nx,ny))
+    return getpath(nx,ny,curr)
+
+def bfs(x,y,ex,ey):
+    global cnt,paths
+    q=deque()
+    q.append((x,y))
+    inq=set()
+    inq.add((x,y,-1,-1))
+    while q:
+        for _ in range(len(q)):
+            x,y=q.popleft()
+            for i in range(8):
+                d1,d2=dx[i],dy[i]
+                if canmove(x,y,d1,d2):
+                    nx,ny=x+d1,y+d2
+                    if 0<=nx<10 and 0<=ny<10 and (nx,ny,x,y) not in inq and s[nx][ny]==0:
+                        q.append((nx,ny))
+                        inq.add((nx,ny,x,y))
+                        ss[nx][ny].append((x,y))
+                        # for i in ss:print(*i)
+                        # print()
+        if len(ss[ex][ey])>1:
+            print(len(ss[ex][ey]))
+            return
+        elif len(ss[ex][ey])==1:
+            path=getpath(ex,ey,[])
+            path='-'.join(map(str,path))
+            for i in path:
+                print(i.strip(),end='')
+            return
+
+bfs(x,y,ex,ey)
