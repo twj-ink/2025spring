46ec948df3198402483228c8c0ca0407ed269ab9
twj-ink <287631943@qq.com>
2025-04-28 14:33:36 +0800

    Upload by Pycharm

--- 2025spring_daily_problems/,,.py
+++ 2025spring_daily_problems/,,.py
@@ -20,30 +20,32 @@ not -1 > 1 -> sum of dif -> 0
 
 not -1 == 0 -> k-(dif(S)-1)
 '''
+from typing import List
+def get_next(s: str) -> List:
+    n=len(s)
+    nxt=[-1]*n
+    j=-1
+    for i in range(1,n):
+        while j!=-1 and s[i]!=s[j+1]:
+            j=nxt[j]
+        if s[i]==s[j+1]:
+            j+=1
+        nxt[i]=j
+    return nxt
+def kmp(s: str, target: str, next: List) -> bool: #返回能否在s中找到target
+    # next = get_next(target)
+    n,m=len(s),len(target)
+    j=0
+    for i in range(n):
+        while j!=-1 and s[i]!=target[j+1]:
+            j=next[j]
+        if s[i]==target[j+1]:
+            j+=1
+        if j==m-1: # 到达了target的末尾
+            return True
+    return False
+s='aabaabaabaab'
+target='aac'
+nxt=get_next(target)
+print(kmp(s,target,nxt))
 
-# def get_next(s):
-#     n=len(s)
-#     nxt=[0]*n
-#     j=-1
-#     for i in range(1,n):
-#         while j!=-1 and s[i]!=s[j+1]:
-#             j=nxt[j]
-#         if s[i]==s[j+1]:
-#             j+=1
-#         nxt[i]=j
-#     return nxt
-# s='aacecaaa'
-# nxt=get_next(s)
-s='aacecaaa'
-n=len(s)
-dp=[[True]*n for _ in range(n)]
-for j in range(1,n):
-    for i in range(j-1,-1,-1):
-        dp[i][j]=dp[i+1][j-1] and s[i]==s[j]
-# for i in dp:print(*i)
-for j in range(n-1,-1,-1):
-    if dp[0][j]:
-        add=s[j+1:]
-        s=add+s
-        print(s)
-        break
