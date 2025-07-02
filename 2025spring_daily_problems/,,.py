4854cb2452d43a8722749afad415aab0e1fb1d06
twj-ink <287631943@qq.com>
2025-07-02 20:22:02 +0800

    Upload by Pycharm

--- 2025spring_daily_problems/,,.py
+++ 2025spring_daily_problems/,,.py
@@ -1,2 +1,31 @@
-from math import  factorial
-print(factorial(5),factorial(10))
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+from decimal import Decimal, getcontext
+
+def compute_power(r_str: str, n: int) -> str:
+    # 1) 计算 R 的有效数字长度，用来估算结果需要的精度
+    digits = r_str.replace('.', '')
+    total_significant = len(digits) * n
+    # 多留一点余量，防止中途运算截断
+    getcontext().prec = total_significant + 5
+
+    # 2) 精确幂运算
+    dec = Decimal(r_str) ** n
+
+    # 3) 转为定点字符串，去掉无用的零
+    s = format(dec, 'f').strip('0').rstrip('.')
+    # 特殊情况：全部被去光了，变成空串，就输出 "0"
+    return s or "0"
+
+def main():
+    for line in sys.stdin:
+        line = line.strip()
+        if not line: 
+            continue
+        r_str, n_str = line.split()
+        print(compute_power(r_str, int(n_str)))
+
+if __name__ == "__main__":
+    main()
--- 2025spring_daily_problems/try.cpp
+++ 2025spring_daily_problems/try.cpp
@@ -1,95 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define debug(...) [](auto...a){ ((cout << a << ' '), ...) << endl;}(#__VA_ARGS__, ":", __VA_ARGS__)
+#define debugv(v) do {cout<< #v <<" : {"; for(int izxc=0;izxc<v.size();++izxc) {cout << v[izxc];if(izxc+1!=v.size()) cout << ","; }cout <<"}"<< endl;} while(0)
+#define debugvv(vv) do {cout << #vv << " = {\n";for (auto &v : vv) {cout << "  { ";for (auto &x : v) cout << x << ' ';cout << "}\n";}cout << "}\n";} while(0)
 
-const int INF = 1e9;
 
-int maxCapacityPath(const unordered_map<int,vector<pair<int,int>>>& g, int s, int t, int n) {
-    vector<int> capacity(n + 1, 0);
-    vector<bool> visited(n + 1, false);
-    capacity[s] = INF;
-    
-    priority_queue<pair<int,int>> pq;
-    pq.push({INF, s});
-    
-    while (!pq.empty()) {
-        int u = pq.top().second;
-        pq.pop();
-        
-        if (visited[u]) continue;
-        visited[u] = true;
-        
-        if (u == t) return capacity[t];
-        
-        if (g.find(u) != g.end()) {
-            for (const auto& p : g.at(u)) {
-                int v = p.first;
-                int w = p.second;
-                if (!visited[v] && capacity[v] < min(capacity[u], w)) {
-                    capacity[v] = min(capacity[u], w);
-                    pq.push({capacity[v], v});
-                }
-            }
-        }
+void solve() {
+    int n, ans = 0;
+    for (int i = 0; i < n; i++) {
+        int a,b;
+        cin >> a >> b;
+        ans += (a < b);
     }
-    return -1;
+    cout << ans << endl;
 }
-    
 
 int main() {
     ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    int t;
-    cin>>t;
-    for (int i = 0; i < t;i++){
-        int op,x;
-        cin>>op>>x;
-        set<int> s;
-        auto lo = s.lower_bound(x);
-        auto it = s.begin();
-        switch (op)
-        {
-        case 5:
-            /* code */
-            s.insert(x);
-            break;
-        case 1:
-            if (s.size()>1) cout << distance(s.begin(), lo) + 2 << endl;
-            else cout << distance(s.begin(), lo) + 1 << endl;
-            break;
-        case 2:
-            auto it = s.begin();
-            for (int i = 0;i<x-1;i++) {
-                it++;
-            }
-            cout<<*it<<endl;
-            break;
-
-        }
-    }
-
-    // int n,m;
-    // cin>>n>>m;
-    // unordered_map<int,vector<pair<int,int>>> g;
-    // for (int i = 0; i < m;i ++) {
-    //     int u,v,w;
-    //     cin>>u>>v>>w;
-    //     g[u].push_back(make_pair(v,w));
-    //     g[v].push_back(make_pair(u,w));
-    // }
-    // unordered_map<int,unordered_map<int,int>> d;
-    // int q;
-    // cin>>q;
-    // for (int j = 0;j<q;j++) {
-    //     int s,e;
-    //     cin>>s>>e;
-    //     if (d.find(s)!=d.end() && d[s].find(e)!=d[s].end()) {
-    //         cout<<d[s][e]<<endl;
-    //         continue;
-    //     }
-    //     int ans = maxCapacityPath(g,s,e,n);
-    //     d[s][e]=ans;
-    //     cout<<ans<<endl;
-    // }
+    cin.tie(0);
     
+    int t;
+    // cin >> t;
+    t=1;
+    while (t--) {
+        solve();
+    }
     return 0;
 }
